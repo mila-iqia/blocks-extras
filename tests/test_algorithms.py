60d41211635256680618dac94a218081ae35ecd7
César Laurent <cesar.laurent@umontreal.ca>
2015-07-29 21:35:28 +0200

    Better testing scheme for NAG.

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -4,6 +4,7 @@ import theano
 from numpy.testing import assert_allclose
 from theano import tensor
 
+from blocks.algorithms import BasicMomentum
 from blocks.extras.algorithms import BasicNesterovMomentum, NesterovMomentum
 from blocks.utils import shared_floatx
 
@@ -14,23 +15,14 @@ def test_basic_nesterov_momentum():
     steps, updates = BasicNesterovMomentum(0.5).compute_steps(
         OrderedDict([(a, tensor.grad(cost, a))]))
     f = theano.function([], [steps[a]], updates=updates)
-    assert_allclose(f()[0], [9., 12.])
-    assert_allclose(f()[0], [10.5, 14.])
-    assert_allclose(f()[0], [11.25, 15.])
-    # Checking equivalence
-    velocity = shared_floatx(a.get_value() * 0.)
-    cost = (a**2).sum()
-    df = tensor.grad(cost, a)
-    delta_v = 0.5*velocity + df
-    velocity_updates = [(velocity, delta_v)]
-    f = theano.function([], [], updates=velocity_updates)
-    f()
-    f()
-    assert_allclose(velocity.get_value(), [9., 12.])
-    f()
-    assert_allclose(velocity.get_value(), [10.5, 14.])
-    f()
-    assert_allclose(velocity.get_value(), [11.25, 15.])
+    steps_classic, updates_classic = BasicMomentum(0.5).compute_steps(
+        OrderedDict([(a, tensor.grad(cost, a))]))
+    f_classic = theano.function([], [steps_classic[a]],
+                                updates=updates_classic)
+    f_classic()  # One call for the "peek ahed" of the Nesterov momentum.
+    assert_allclose(f()[0], f_classic()[0])
+    assert_allclose(f()[0], f_classic()[0])
+    assert_allclose(f()[0], f_classic()[0])
 
 
 def test_nesterov_momentum():
