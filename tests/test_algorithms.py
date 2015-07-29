2b89c11ce48550a4b54554a31e7eb947b161d72a
César Laurent <cesar.laurent@umontreal.ca>
2015-07-29 21:06:15 +0200

    Added equivalence test.

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -17,6 +17,20 @@ def test_basic_nesterov_momentum():
     assert_allclose(f()[0], [9., 12.])
     assert_allclose(f()[0], [10.5, 14.])
     assert_allclose(f()[0], [11.25, 15.])
+    # Checking equivalence
+    velocity = shared_floatx(a.get_value() * 0.)
+    cost = (a**2).sum()
+    df = tensor.grad(cost, a)
+    delta_v = 0.5*velocity + df
+    velocity_updates = [(velocity, delta_v)]
+    f = theano.function([], [], updates=velocity_updates)
+    f()
+    f()
+    assert_allclose(velocity.get_value(), [9., 12.])
+    f()
+    assert_allclose(velocity.get_value(), [10.5, 14.])
+    f()
+    assert_allclose(velocity.get_value(), [11.25, 15.])
 
 
 def test_nesterov_momentum():
