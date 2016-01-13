13bcf09777835856296682423298ebf9e7382bbe
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2016-01-13 16:24:38 -0500

    Remove namespace from setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import find_packages, setup
 
 setup(
     name='blocks_extras',
-    namespace_packages=['blocks'],
     install_requires=['blocks'],
     packages=find_packages(),
     scripts=['bin/blocks-plot'],
